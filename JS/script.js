c791dd2347fb7f7e6197cfa0502cd474dcce8b9d
LorenzoMonteforte92 <l.monteforte@yahoo-it>
2024-03-15 17:24:12 +0100

    feat: add squares generated after click

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,37 @@
 // L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
 // Ogni cella ha un numero progressivo, da 1 a 100.
 // Ci saranno quindi 10 caselle per ognuna delle 10 righe.
-// Quando l'utente clicca su ogni cella, la cella cliccata 
-//si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+// Quando l'utente clicca su ogni cella, 
+// la cella cliccata si colora di azzurro ed emette un messaggio in console con il numero della cella cliccata.
+
+//selezionare il bottone nel DOM e inserirlo in una variabile
+//metterlo in attesa di un evento click
+let startBtn = document.querySelector(`#btn`);
+startBtn.addEventListener(`click`, function(){
+    //generare una griglia di 100 elementi con numeri da 1 a 100
+    //selezionare il contenitore
+    let boxContainer = document.querySelector(`#container`);
+    //stamparci dentro gli elementi usando una funzione
+    for(let i = 1; i <= 100; i++){
+         let newSquare = generateSquare (i);
+         boxContainer.append(newSquare);
+    }
+}, {once : true})
+
+// ----------------
+//     FUNCTIONS
+// ----------------
+
+//funzione che genera un quadrato
+//number --> numero all'interno dei quadrati
+//return: un elemento del DOM che rappresenta un quadrato
+function generateSquare (number){
+    let square = document.createElement(`div`);
+    square.classList.add(`box`);
+    square.innerHTML += `<span>${number}</span>`
+    return square
+}
+
+function changeColorClick (){
+
+}
